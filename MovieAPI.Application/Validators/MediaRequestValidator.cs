25fc444d98f4df045c63d7c53bf8133ece21ae08
rubangarmon <rubangarmon99@gmail.com>
2025-02-22 21:03:02 -0600

    update user docker

--- MovieAPI.Application/Validators/MediaRequestValidator.cs
+++ MovieAPI.Application/Validators/MediaRequestValidator.cs
@@ -1,15 +1,14 @@
 ﻿using FluentValidation;
 using MovieAPI.Application.ContractsModels;
 
-namespace MovieAPI.Application.Validators
+namespace MovieAPI.Application.Validators;
+
+public class MediaRequestValidator : AbstractValidator<MediaRequest>
 {
-    public class MediaRequestValidator : AbstractValidator<MediaRequest>
-    {
-        public MediaRequestValidator() 
-        { 
-            RuleFor(x => x.Name).NotNull();
-            RuleFor(x => x.Page)
-                .GreaterThan(0);
-        }
+    public MediaRequestValidator() 
+    { 
+        RuleFor(x => x.Name).NotNull();
+        RuleFor(x => x.Page)
+            .GreaterThan(0);
     }
 }
